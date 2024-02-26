a30b4310f098007ac28c16f8ef5c746c6fb010db
Yadvir Kaur <yadvirkaur15@gmail.com>
2024-02-26 12:40:57 +0530

    Apply Blurhash on images

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,14 +1,32 @@
+import React from 'react';
 import { Card, CardBody, Heading, Image } from '@chakra-ui/react';
 import { Photo } from '../hooks/useImages';
+import { Blurhash } from 'react-blurhash';
 
 interface Props {
   image: Photo;
 }
 
 const ImageCard = ({ image }: Props) => {
+  const [imageLoaded, setImageLoaded] = React.useState<boolean>(false);
   return (
     <Card>
-      <Image src={image.urls.thumb}></Image>
+      {image.blur_hash && !imageLoaded && (
+        <Blurhash
+          hash={image.blur_hash}
+          width="200"
+          height="300px"
+          resolutionX={32}
+          resolutionY={32}
+          punch={1}
+        />
+      )}
+      <Image
+        src={image.urls.thumb}
+        alt={image.alt_description}
+        onLoad={() => setImageLoaded(true)}
+        display={imageLoaded ? 'block' : 'none'}
+      />
       <CardBody>
         <Heading fontSize="2xl">{image.alt_description}</Heading>
       </CardBody>
