faed3022a23d5704ddca34b44e30604ab3dc74c2
Yadvir Kaur <yadvirkaur15@gmail.com>
2024-03-04 12:29:44 +0530

    Refactor- solve 404 error and add navigate

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,17 +1,20 @@
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
 import { useRef } from 'react';
 import { BsSearch } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
 import useImageQueryStore from '../store';
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
   const setSearchText = useImageQueryStore((s) => s.setSearchText);
+  const navigate = useNavigate();
 
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
         if (ref.current) setSearchText(ref.current.value);
+        navigate(`/`);
       }}
     >
       <InputGroup>
